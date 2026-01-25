a1366df8125dd271ec9d4bd7459aa6edf67ede4f
MATEQH <mate.molnar321@gmail.com>
2026-01-25 12:55:18 +0100

    feat: add binFracToDecFrac

---
+++ binFracToDecFrac.js
@@ -0,0 +1,18 @@
+function binFracToDec(binFrac) {
+    var decFrac = 0;
+    var hatvany = 1 / 2;
+    var maxFrac = 15;
+
+    for (let i = 0; i < maxFrac; i++) {
+        decFrac += binFrac[i] * hatvany;
+        hatvany /= 2;
+    }
+
+    return decFrac;
+}
+
+var binFrac = "0110010111000110";
+var decFrac = binFracToDec(binFrac);
+
+console.log("Bináris tört: 0." + binFrac);
+console.log("Decimális tört: " + decFrac);
